9e7106f0b5f952d4269247e71cbfa592dfb462f0
Vyshali <124905214+Vyshhh@users.noreply.github.com>
2026-03-11 20:01:45 +0530

    Updated

---
+++ user_details/src/main/java/com/example/quiz/dto/dto/Userdto.java
@@ -0,0 +1,19 @@
+package com.example.quiz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@AllArgsConstructor
+
+public class Userdto {
+
+    private Integer id;
+
+    private String name;
+    private String username;
+    private String email;
+
+
+}
